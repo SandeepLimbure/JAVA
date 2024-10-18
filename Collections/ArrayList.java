ee32debf9d38cd287bec92d59d5517c71bfa557b
sandeep limbure <127886236+SandeepLimbure@users.noreply.github.com>
2024-10-19 00:01:09 +0530

    Add files via upload

---
+++ Collections/ArrayList.java
@@ -0,0 +1,13 @@
+package Collections.ArrayList;
+
+import java.util.*;
+
+public class ArrayList {
+    public static void main(String[] args) {
+        List<String> res = new java.util.ArrayList<>();
+        res.add("Red");
+        res.add("Green");
+        res.add("Orange");
+        System.out.println(res);
+    }
+}
